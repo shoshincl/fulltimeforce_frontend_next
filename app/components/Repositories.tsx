7b6235f47cc61040fce85b9d01260bbc2a748fdd
Diego Robles <shoshincl@gmail.com>
2023-07-26 19:36:32 -0400

    feat(main):show commit history

--- app/components/Repositories.tsx
+++ app/components/Repositories.tsx
@@ -6,7 +6,7 @@ function Repositories({ repositories }: any) {
   return (
     <div className="grid grid-cols-1 gap-2 w-[100%] md:max-w-[800px]">
       {repositories.map(({ name, topics, pushed_at }: any) => (
-        <Link href={`/commits/${name}`}>
+        <Link href="/repository/[name]" as={`/repository/${name}`}>
           <div className="bg-white hover:cursor-pointer hover:shadow-md p-4 rounded-md">
             <div className="flex flex-col">
               <p className="text-gray-500">Name:</p>
---
+++ app/repository/[name]/page.tsx
@@ -0,0 +1,35 @@
+import moment from "moment";
+
+const getCommits = (name: string) =>
+  fetch(`${process.env.API_BASE_URL}/repository/${name}`).then((res) =>
+    res.json()
+  );
+
+export default async function Repository({ params }: any) {
+  const { name } = params;
+  const commits = await getCommits(name);
+  return (
+    <main className="grid grid-cols-1 gap-4 min-h-screen flex-col items-center p-24">
+      <p className="text-2xl font-bold">{name}</p>
+      <p className="text-lg font-bold">Commit History:</p>
+      {commits.map(({ sha, commit, author }: any) => (
+        <div className="bg-white hover:shadow-md p-4 rounded-md">
+          <p className="text-gray-500">{sha}</p>
+          <div className="flex">
+            <p className="text-md text-gray-500">Message:</p>
+            <p className="text-md font-semibold">{commit.message}</p>
+          </div>
+          <div className="flex">
+            <p className="text-sm text-gray-500">Author:</p>
+            <p className="text-sm font-semibold">{author.login}</p>
+          </div>
+          <div className="flex">
+            <p className="text-sm text-gray-500">
+              {moment(commit.author.date).fromNow()}
+            </p>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
+}
